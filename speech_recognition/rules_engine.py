cb5afe74b0dd27f5c17367b8fc1ba9e92ccc024c
Michael Bentivegna <mjbentivegna01@gmail.com>
2023-03-18 22:03:25 -0400

    path algo except captures

--- speech_recognition/rules_engine.py
+++ speech_recognition/rules_engine.py
@@ -1,10 +1,165 @@
 # %% Libraries
 
+from numpy import square
 import chess
 import chess.svg
-from IPython.display import SVG
-import speech
+# from IPython.display import SVG
+# import speech
 import time
+
+mapping = {
+    'a': 0,
+    'b': 1,
+    'c': 2,
+    'd': 3,
+    'e': 4,
+    'f': 5,
+    'g': 6,
+    'h': 7
+}
+baseline_coord = (0,0)
+
+# TEMPORARY FUNCTIONS FOR PRINTING
+# Motor 1 = moves x axis
+# Motor 2 = moves y axis
+# Direction: 1 means to the right (or up), 0 means to the left (or down)
+def move_num_squares(motor=1, _dir=1, num_squares=1):
+    tmp = "Up"
+    if motor == 1:
+        if _dir == 1:
+            tmp = "Right"
+        else:
+            tmp = "Left"
+    else:
+        if _dir == 0:
+            tmp = "Down"
+        
+    
+    print("Move " + str(num_squares) + " " + tmp)
+
+def move_num_squares_diagonal(_dir_1=1, _dir_2=1, num_squares=1):
+    tmp = "Up-Left"
+    if _dir_1 == 1:
+        if _dir_2 == 1:
+            tmp = "Up-Right"
+        else:
+            tmp = "Down-Right"
+    else:
+        if _dir_2 == 0:
+            tmp = "Down-Left"
+            
+    print("Move " + str(num_squares) + " " + tmp)
+    
+def magnet_on():
+    print("Magnet On")
+
+def magnet_off():
+    print("Magnet Off")
+  
+  
+# PERMANENT FUNCTIONS
+def move_num_squares_helper(distance, x_dir):
+    if x_dir:
+        if distance < 0:
+            move_num_squares(1, 0, abs(distance))
+        else:
+            move_num_squares(1, 1, distance)
+    else:
+        if distance < 0:
+            move_num_squares(2, 0, abs(distance))
+        else:
+            move_num_squares(2, 1, distance)
+
+def move_num_squares_diagnol_helper(x_val, y_val):
+    if x_val < 0:
+        if y_val < 0:
+            move_num_squares_diagonal(0, 0, abs(x_val))
+        else:
+            move_num_squares_diagonal(0, 1, abs(x_val))
+    else:
+        if y_val < 0:
+            move_num_squares_diagonal(1, 0, abs(x_val))
+        else:
+            move_num_squares_diagonal(1, 1, abs(x_val))
+
+# Assume we start in the middle of square a1 (bottom left of the board)
+def execute_move(m):
+    start_coord = (mapping[m[0][0]], int(m[0][1]) - 1)
+    end_coord = (mapping[m[1][0]], int(m[1][1]) - 1)
+    
+    if m[2] == True:
+        # Go to end_coord, raise magnet
+        # Go down 1/2, then off the board
+        # Then go back to baseline_coord
+        print("Capture Portion Needs to be Programmed Later")
+    
+    # Go to start square and raise magnet
+    move_num_squares(1, 1, start_coord[0])
+    move_num_squares(2, 1, start_coord[1])
+    magnet_on()
+    
+    # Check if knight
+    if m[3] == 2:
+        # Move Knight
+        if abs(end_coord[0] - start_coord[0]) == 1:
+            move_num_squares_helper((end_coord[0] - start_coord[0]) / 2, True)
+            move_num_squares_helper(end_coord[1] - start_coord[1], False)
+            move_num_squares_helper((end_coord[0] - start_coord[0]) / 2, True)
+        else:
+            move_num_squares_helper((end_coord[1] - start_coord[1]) / 2, False)
+            move_num_squares_helper(end_coord[0] - start_coord[0], True)
+            move_num_squares_helper((end_coord[1] - start_coord[1]) / 2, False)
+            
+        # Turn magnet off and go to baseline square
+        magnet_off()
+        move_num_squares(1, 0, end_coord[0])
+        move_num_squares(2, 0, end_coord[1])
+        
+    # Check if kingside castle
+    elif (m[3] == 6 and start_coord == (4, 0) and end_coord == (6, 0)) or (m[3] == 6 and start_coord == (4, 7) and end_coord == (6, 7)):
+        move_num_squares(1, 1, 2)
+        magnet_off()
+        move_num_squares(1, 1, 1)
+        magnet_on()
+        move_num_squares(2, 0, .5)
+        move_num_squares(1, 0, 2)
+        move_num_squares(2, 1, .5)
+        
+        magnet_off()
+        move_num_squares(1, 0, end_coord[0] - 1)
+        move_num_squares(2, 0, end_coord[1])
+         
+    # Check if queenside castle
+    elif (m[3] == 6 and start_coord == (4, 0) and end_coord == (2, 0)) or (m[3] == 6 and start_coord == (4, 7) and end_coord == (2, 7)):
+        move_num_squares(1, 0, 2)
+        magnet_off()
+        move_num_squares(1, 0, 2)
+        magnet_on()
+        move_num_squares(2, 0, .5)
+        move_num_squares(1, 1, 3)
+        move_num_squares(2, 1, .5)
+        
+        magnet_off()
+        move_num_squares(1, 0, end_coord[0] + 1)
+        move_num_squares(2, 0, end_coord[1])
+        
+    else:
+        # Move piece
+        # Only y-movement
+        if end_coord[0] - start_coord[0] == 0:
+            move_num_squares_helper(end_coord[1] - start_coord[1], False)
+        # Only x-movement
+        elif end_coord[1] - start_coord[1] == 0:    
+            move_num_squares_helper(end_coord[0] - start_coord[0], True)
+        # Diagnol movement
+        else:
+            move_num_squares_diagnol_helper(end_coord[0] - start_coord[0], end_coord[1] - start_coord[1])
+        
+        # Turn magnet off and go to baseline square
+        magnet_off()
+        move_num_squares(1, 0, end_coord[0])
+        move_num_squares(2, 0, end_coord[1])
+        
 
 def make_move(board, move):
     if board.is_checkmate():
@@ -16,8 +171,7 @@ def make_move(board, move):
     #     print("Black's move: \n")
     
     # moves = board.legal_moves
-    s = str(board.legal_moves)
-    s = s[s.find("(")+1:s.find(")")]
+
     try:
         #move = input("Enter a move: ")
         
@@ -26,12 +180,22 @@ def make_move(board, move):
 
         board.push_san(move)
 
-        gantry_move_format = (square_diction.uci()[:2], square_diction.uci()[2:], is_cap)
+        gantry_move_format = (square_diction.uci()[:2], square_diction.uci()[2:4], is_cap, board.piece_at(chess.parse_square(square_diction.uci()[2:4])).piece_type)
+        s = str(board.legal_moves)
+        s = s[s.find("(")+1:s.find(")")]
         print(gantry_move_format)
         print(board)
+        print(s)
+        execute_move(gantry_move_format)
     except ValueError:
         print("\nEnter a valid move...")
-    
 
+
+# Temporary for path algo testing
+board = chess.Board()
+print(board)
+while(True):
+    move = input("What is your chess move?")
+    make_move(board, move)
 
 
